54008d59c3549cdb635e1fa5c1a04cae4ca76a70
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-01-28 15:42:52 +0100

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Contributing
+
+Want to help improve this plugin? Great!  
+Project is open-source so fork repo and join us!
+
+## License
+
+MIT License © [Sparing Interactive](https://github.com/SparingSoftware)
---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+require('dotenv').config()
+const chalk = require('chalk')
+
+const fs = require('fs')
+const path = require('path')
+const generateApi = require('swagger-typescript-api').generateApi
+
+function main() {
+  if (!process.env.OPEN_API_URL) {
+    console.log(
+      chalk.yellow('OPEN_API_URL is not defined. Service creation aborted!')
+    )
+    return
+  }
+
+  const OUTPUT_NAME = 'index.ts'
+  const OUTPUT_PATH = path.resolve(process.cwd(), './service/')
+  const TEMPLATES_PATH = path.resolve(__dirname, './templates/')
+
+  generateApi({
+    name: OUTPUT_NAME,
+    url: process.env.OPEN_API_URL,
+    templates: TEMPLATES_PATH
+  }).then(({ files }) => {
+    if (!fs.existsSync(OUTPUT_PATH)) fs.mkdirSync(OUTPUT_PATH)
+
+    files.forEach(({ content, name }) => {
+      fs.writeFileSync(`${OUTPUT_PATH}/${name}`, content)
+    })
+  })
+}
+
+main()
