61769d34a0ad6f212a55e055256328cd127e2501
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-03-25 11:40:58 +0100

    Update to swagget-typescript-api 7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 
 const fs = require('fs')
 const path = require('path')
-const generateApi = require('swagger-typescript-api').generateApi
+const { generateApi } = require('swagger-typescript-api')
 
 function main() {
   if (!process.env.OPEN_API_URL) {
@@ -14,19 +14,34 @@ function main() {
     return
   }
 
-  const OUTPUT_NAME = 'index.ts'
+  const OUTPUT_NAME = '__generated-api.ts'
   const OUTPUT_PATH = path.resolve(process.cwd(), './service/')
-  const TEMPLATES_PATH = path.resolve(__dirname, './templates/')
 
   generateApi({
     name: OUTPUT_NAME,
     url: process.env.OPEN_API_URL,
-    templates: TEMPLATES_PATH
+    httpClientType: 'axios',
+    prettier: {
+      parser: 'typescript',
+      semi: false,
+      arrowParens: 'avoid',
+      trailingComma: 'none',
+      singleQuote: true,
+      endOfLine: 'lf',
+      bracketSpacing: true,
+      printWidth: 80,
+      useTabs: false,
+      quoteProps: 'as-needed',
+      tabWidth: 2
+    }
   }).then(({ files }) => {
     if (!fs.existsSync(OUTPUT_PATH)) fs.mkdirSync(OUTPUT_PATH)
 
     files.forEach(({ content, name }) => {
-      fs.writeFileSync(`${OUTPUT_PATH}/${name}`, content)
+      fs.writeFileSync(
+        `${OUTPUT_PATH}/${name}`,
+        `${content}\nexport const api = new Api()\n`
+      )
     })
   })
 }
