4dba054cc774d6fc4c08b270524aaaafefaf38de
Tomasz Chmielnicki <t.chmielnicki@p-programisci.pl>
2024-08-12 11:54:34 +0200

    feat: Accept fetchKeys array

--- test/__generated-api.ts
+++ test/__generated-api.ts
@@ -1666,7 +1666,7 @@ type MapObjectKeysToTrue<ObjectType extends Record<PropertyKey, any>> = {
     : true
 }
 type FetchKeysType = { [key in PropertyKey]: true | FetchKeysType }
-export type FetchKeys<ResponseModel> = ResponseModel extends {
+type FetchKeysObject<ResponseModel> = ResponseModel extends {
   count?: number
   results?: Array<infer DataModel>
 }
@@ -1698,7 +1698,7 @@ type NestedSafePick<ResponseModel, FetchKeysType> = ResponseModel extends Array<
         : never
     }
 
-export type PickKeys<ResponseModel, FetchKeysType> = {} extends FetchKeysType
+type PickKeysFromObject<ResponseModel, FetchKeysType> = {} extends FetchKeysType
   ? ResponseModel
   : ResponseModel extends {
       count?: number
@@ -1713,29 +1713,104 @@ export type PickKeys<ResponseModel, FetchKeysType> = {} extends FetchKeysType
   ? NestedSafePick<DataModel, FetchKeysType>[]
   : NestedSafePick<ResponseModel, FetchKeysType>
 
-export function postprocessQuery(query?: any) {
-  if (!query) return query
-
-  const parsedQuery = query
-  if (query.fetchKeys) {
-    function getStringifiedQueryKeys(keys: FetchKeysType) {
-      let query = '{'
-      Object.entries(keys).forEach(([key, value], index) => {
-        if (typeof value === 'object') {
-          const keyToAdd = `${key}${getStringifiedQueryKeys(value)}`
-          query += index > 0 ? `,${keyToAdd}` : keyToAdd
-        } else {
-          query += index > 0 ? `,${key}` : key
-        }
-      })
-      return query + '}'
+function getStringifiedQueryKeys(keys: FetchKeysObject<unknown>) {
+  let query = '{'
+  Object.entries(keys as FetchKeysType).forEach(([key, value], index) => {
+    if (typeof value === 'object') {
+      const keyToAdd = `${key}${getStringifiedQueryKeys(value)}`
+      query += index > 0 ? `,${keyToAdd}` : keyToAdd
+    } else {
+      query += index > 0 ? `,${key}` : key
     }
+  })
+  return query + '}'
+}
 
-    parsedQuery.query = getStringifiedQueryKeys(query.fetchKeys)
+type IsArray<T> = T extends Array<any> ? true : false
+
+type DotNotationKeys<T, P extends string = ''> = T extends object
+  ? IsArray<T> extends true
+    ? P extends ''
+      ? never
+      : P
+    : {
+        [K in keyof T]: K extends string
+          ? P extends ''
+            ? K | DotNotationKeys<T[K], K>
+            : `${P}.${K}` | DotNotationKeys<T[K], `${P}.${K}`>
+          : never
+      }[keyof T]
+  : P
+
+type FetchKeysArray<T> = DotNotationKeys<T>[]
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never
+
+type Split<S extends string, D extends string> = string extends S
+  ? string[]
+  : S extends ''
+  ? []
+  : S extends `${infer T}${D}${infer U}`
+  ? [T, ...Split<U, D>]
+  : [S]
+
+type PickByPath<T, Path extends any[]> = Path extends [infer P, ...infer Rest]
+  ? P extends keyof T
+    ? { [K in P]: PickByPath<T[P], Rest> }
+    : unknown
+  : T
+
+type PickKeysFromArray<T, Keys extends string[]> = Merge<
+  UnionToIntersection<
+    {
+      [K in keyof Keys]: PickByPath<T, Split<Extract<Keys[K], string>, '.'>>
+    }[number]
+  >
+>
+
+type Merge<T> = {
+  [K in keyof T]: T[K]
+}
+
+function getFetchKeysFromArray(paths: string[]) {
+  const fetchKeys: FetchKeysType = {}
+  for (const path of paths) {
+    let currentObject = fetchKeys
+    const nestedFields = path.split('.')
+    for (const nestedField of nestedFields.slice(0, -1)) {
+      if (!currentObject[nestedField]) currentObject[nestedField] = {}
+      currentObject = currentObject[nestedField] as FetchKeysType
+    }
+    currentObject[nestedFields.at(-1) as string] = true
   }
+  return getStringifiedQueryKeys(fetchKeys)
+}
 
-  delete parsedQuery.fetchKeys
-  return parsedQuery
+export type FetchKeys<ResponseModel = unknown> =
+  | FetchKeysObject<ResponseModel>
+  | FetchKeysArray<ResponseModel>
+export type PickKeys<
+  ResponseModel,
+  KeysToFetch extends FetchKeys<ResponseModel>
+> = KeysToFetch extends string[]
+  ? PickKeysFromArray<ResponseModel, KeysToFetch>
+  : PickKeysFromObject<ResponseModel, KeysToFetch>
+
+function getQuery(fetchKeys: FetchKeys) {
+  return Array.isArray(fetchKeys)
+    ? getFetchKeysFromArray(fetchKeys)
+    : getStringifiedQueryKeys(fetchKeys)
+}
+
+export function postprocessQuery(params?: any) {
+  if (!params) return params
+  if (params.fetchKeys) params.query = getQuery(params.fetchKeys)
+  delete params.fetchKeys
+  return params
 }
 
 /**
