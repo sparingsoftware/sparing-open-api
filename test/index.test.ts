7e9a3b0c8b2d81aab4bdd214c2bb355b6fb7d84c
Tomasz Chmielnicki <t.chmielnicki@p-programisci.pl>
2024-08-21 14:47:27 +0200

    feat: Validate fetchKeys array

--- test/index.test.ts
+++ test/index.test.ts
@@ -83,6 +83,16 @@ describe('postprocessQuery', () => {
       })
       expect(parsedQuery?.query).toEqual('{key1{key2,key3}}')
     })
+    it('throws when there are duplicate keys', () => {
+      expect(() => {
+        postprocessQuery({ fetchKeys: ['key1', 'key1'] })
+      }).toThrow(`Duplicate`)
+    })
+    it('throws when there is invalid duplicate nesting', () => {
+      expect(() => {
+        postprocessQuery({ fetchKeys: ['key1', 'key1.key2'] })
+      }).toThrow('Invalid')
+    })
   })
 })
 
