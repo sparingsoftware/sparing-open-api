42402cf97ef2436349b25e5d37d4e85363a2ba23
Tomasz Chmielnicki <t.chmielnicki@p-programisci.pl>
2025-04-08 16:15:07 +0200

    test: Add test for generating from url

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,25 +2,53 @@ import { describe, expect, expectTypeOf, it, test } from 'vitest'
 import { generateFromConfig } from '../src/generateFromConfig'
 import fs from 'fs/promises'
 import path from 'path'
-import { PickKeys, FetchKeys, postprocessQuery, Api } from './__generated-api'
+import {
+  PickKeys,
+  FetchKeys,
+  postprocessQuery,
+  Api
+} from './generated/__generated-api'
 import onCreateRoute from '../src/onCreateRoute'
 
-const GENERATED_DIRECTORY = 'test'
-const GENERATED_FILENAME = '__generated-api.ts'
-const GENERATED_PATH = path.join(GENERATED_DIRECTORY, GENERATED_FILENAME)
+const GENERATED_DIRECTORY = path.join('test', 'generated')
 
-test('generates api from config object', async () => {
-  await fs.rm(GENERATED_PATH, {
-    force: true
+describe('generates api from config object', () => {
+  it('by file path', async () => {
+    const GENERATED_FILENAME = '__generated-api.ts'
+    const GENERATED_PATH = path.join(GENERATED_DIRECTORY, GENERATED_FILENAME)
+
+    await fs.rm(GENERATED_PATH, {
+      force: true
+    })
+
+    await generateFromConfig({
+      filePath: path.join('test', 'api.json'),
+      outDir: GENERATED_DIRECTORY
+    })
+
+    const generated = await fs.readFile(GENERATED_PATH, { encoding: 'utf-8' })
+    expect(generated).toMatchSnapshot()
   })
+  it('by url', async () => {
+    const GENERATED_FILENAME = '__generated-api-from-url.ts'
+    const GENERATED_PATH = path.join(GENERATED_DIRECTORY, GENERATED_FILENAME)
+    const TEST_API_URL = 'https://petstore3.swagger.io/api/v3/openapi.json'
 
-  await generateFromConfig({
-    filePath: path.join('test', 'api.json'),
-    outDir: GENERATED_DIRECTORY
+    await fs.rm(GENERATED_PATH, {
+      force: true
+    })
+
+    await generateFromConfig({
+      url: TEST_API_URL,
+      outDir: GENERATED_DIRECTORY,
+      outFilename: GENERATED_FILENAME
+    })
+
+    const generated = await fs.readFile(GENERATED_PATH, { encoding: 'utf-8' })
+    expect(generated).toContain('type FetchKeys')
+
+    await fs.rm(GENERATED_PATH)
   })
-
-  const generated = await fs.readFile(GENERATED_PATH, { encoding: 'utf-8' })
-  expect(generated).toMatchSnapshot()
 })
 
 describe('postprocessQuery', () => {
